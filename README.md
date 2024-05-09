e7e206d4c0fe135ef9d14e99475ecc4db97f52ca
Bedo Eid <65284648+Abdo-Eid@users.noreply.github.com>
2024-05-09 22:44:02 +0300

    prepare for upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 ## to do
+- [ ] read the load data file
+- [ ] make it a grid with 3 frames  
+- [ ] make the nan and duplicate count for each column which will result in dataframe
+- [ ] use tksheet to show the dataframes
+- [ ] make utilits and windows for each 
+
+## installation
+1. install librarys in requiement.txt
+2. run the app.py
--- pages/preprocessing.py
+++ pages/preprocessing.py
@@ -1,10 +1,18 @@
 import tkinter as tk   
-
+from .utilits import simpel_imputer, min_max, apply_smote, encoding_option
 
 class PreProcessing(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
         tk.Button(self, text="Main", command=lambda: parent.show_page("Main")).place(x = 5, y = 5)
+
+
+        frame = tk.Frame(self)
+        tk.Button(frame, text="Simple Imputer", command=simpel_imputer).pack(side=tk.TOP, pady=5)
+        tk.Button(frame, text="Minimax Scaler", command=min_max).pack(side=tk.TOP, pady=5)
+        tk.Button(frame, text="Encoding", command=encoding_option).pack(side=tk.TOP, pady=5)
+        tk.Button(frame, text="Smote", command=apply_smote).pack(side=tk.TOP, pady=5)
+        frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
           
