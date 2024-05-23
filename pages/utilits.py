24f336ea341948c2662f0c9529eae7d462d2447d
omarhamed888 <120338065+omarhamed888@users.noreply.github.com>
2024-05-23 17:47:03 +0300

    svm classification works

--- pages/utilits.py
+++ pages/utilits.py
@@ -7,7 +7,9 @@ from imblearn.over_sampling import SMOTE
 import pandas as pd
 import tkinter as tk
 from .shared import DataModel
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, \
+    classification_report
+from sklearn.neighbors import KNeighborsClassifier
 
 
 # ------------------- Pre Processing -------------------
@@ -25,15 +27,27 @@ def label_encode(data_model: DataModel):
     y = data_model.df.iloc[:, -1]
 
     y = LabelEncoder().fit_transform(y)
-
-    data_model.df = pd.concat([X, pd.DataFrame(y, columns=['lable'])], axis=1)
+    y = y.astype(int)
+    data_model.df = pd.concat([X, pd.DataFrame(y, columns=['label'])], axis=1)
 
 
 def one_hot_encode(data_model: DataModel):
-    categorical_columns = data_model.df.select_dtypes(include=['object']).columns
+    categorical_columns = data_model.df.select_dtypes(include=['object']).columns.tolist()
+    categorical_columns = [col for col in categorical_columns if col not in ["label"]]
 
-    ct = ColumnTransformer([('encoder', OneHotEncoder(), categorical_columns)], remainder='passthrough')
-    data_model.df = pd.DataFrame(ct.fit_transform(data_model.df))
+    ct = ColumnTransformer(
+        [('encoder', OneHotEncoder(), categorical_columns)],
+        remainder='passthrough'
+    )
+
+    transformed_features = ct.fit_transform(data_model.df)
+
+    feature_names = ct.get_feature_names_out()
+    transformed_df = pd.DataFrame(transformed_features, columns=feature_names)
+
+    # Add the label column back to the transformed DataFrame
+    transformed_df['label'] = data_model.df['label'].astype(int)
+    data_model.df = transformed_df
 
 
 def min_max(data_model: DataModel):
@@ -63,109 +77,25 @@ def SVM_C(data_model: DataModel, kernel, size):
     # Train the classifier
     svm_classifier.fit(X_train, y_train)
     y_pred = svm_classifier.predict(X_test)
-
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
-    confusion_mat = confusion_matrix(y_test, y_pred)
-    accuracy = accuracy_score(y_test, y_pred)
-    display_evaluation_metrics(accuracy, precision, recall, f1, confusion_mat)
+    display_evaluation_metrics(y_test, y_pred, "c")
 
 
-# def KNN(data_model: DataModel,strategy):
-#     label = Label(frame2, text="Enter Number of Neighbors:")
-#     label.pack()
-#     entry = Entry(frame2, width=30)
-#     entry.pack()
-#     button = Button(frame2, text="Submit Number of Neighbors", command=lambda: print(get_input(entry)))
-#     button.pack()
 
-#     label = Label(frame2, text="Enter Distance Metric (metric eg :: (hamming,mahalanobis,chebyshev,....):")
-#     label.pack()
-#     entry2 = Entry(frame2, width=30)
-#     entry2.pack()
-#     button = Button(frame2, text="Submit Distance Metric", command=lambda: print(get_input(entry2)))
-#     button.pack()
-
-#     label3 = Label(frame2, text="Enter Train Ratio:")
-#     label3.pack()
-#     entry3 = Entry(frame2, width=30)
-#     entry3.pack()
-#     button4 = Button(frame2, text="Submit Ratio", command=lambda: print(get_input(entry3)))
-#     button4.pack()
-
-#     button = Button(frame2, text="implement", command=lambda :calll())
-#     button.pack()
-
-#     def calll():
-#         X_train, X_test, y_train, y_test = train_test_split(data.iloc[:, :-1], data.iloc[:, -1],test_size=1 - float(entry3.get()) / 100,random_state=42)
-#         # Initialize SVM classifier
-#         KNN_classifier = KNeighborsClassifier(n_neighbors=int(entry.get()),metric=entry2.get())
-#         # Train the classifier
-#         KNN_classifier.fit(X_train, y_train)
-#         # Predict on the test set
-#         y_pred = KNN_classifier.predict(X_test)
-#         # Calculate accuracy
-#         precision = precision_score(y_test, y_pred)
-#         recall = recall_score(y_test, y_pred)
-#         f1 = f1_score(y_test, y_pred)
-#         confusion_mat = confusion_matrix(y_test, y_pred)
-#         accuracy = accuracy_score(y_test, y_pred)
-#         # Create a Tkinter window
-#         display_evaluation_metrics(accuracy, precision, recall, f1, confusion_mat)
-# def DTC():
-#     global data
-#     frame2 = Toplevel()
-#     frame2.title("DTC Options")
-#     frame2.geometry("600x400")
-
-#     def get_input(entry):
-#         strategy = entry.get()
-#         return strategy
-
-#     label = Label(frame2, text="Enter Max Depth:")
-#     label.pack()
-#     entry = Entry(frame2, width=30)
-#     entry.pack()
-#     button = Button(frame2, text="Submit Depth", command=lambda: print(get_input(entry)))
-#     button.pack()
-
-#     label = Label(frame2, text="Enter Criterion (e.g., gini or entropy):")
-#     label.pack()
-#     entry2 = Entry(frame2, width=30)
-#     entry2.pack()
-#     button = Button(frame2, text="Submit Criterion", command=lambda: print(get_input(entry2)))
-#     button.pack()
-#     label3 = Label(frame2, text="Enter Train Ratio:")
-#     label3.pack()
-#     entry3 = Entry(frame2, width=30)
-#     entry3.pack()
-#     button4 = Button(frame2, text="Submit Criterion", command=lambda: print(get_input(entry3)))
-#     button4.pack()
-#     button = Button(frame2, text="implement", command=lambda:call())
-#     button.pack()
-
-#     def call():
-#         X_train, X_test, y_train, y_test = train_test_split(data.iloc[:, :-1], data.iloc[:, -1],
-#                                                             test_size=1 - float(entry3.get()) / 100,
-#                                                             random_state=42)
-
-#         # Initialize SVM classifier
-#         DTC_classifier = DecisionTreeClassifier(max_depth=int(entry.get()),criterion=entry2.get())
-#         # Train the classifier
-#         DTC_classifier.fit(X_train, y_train)
-#         # Predict on the test set
-#         y_pred = DTC_classifier.predict(X_test)
-#         # Calculate accuracy
-#         precision = precision_score(y_test, y_pred)
-#         recall = recall_score(y_test, y_pred)
-#         f1 = f1_score(y_test, y_pred)
-#         confusion_mat = confusion_matrix(y_test, y_pred)
-#         accuracy = accuracy_score(y_test, y_pred)
-#         display_evaluation_metrics(accuracy, precision, recall, f1, confusion_mat)
+def KNN(data_model: DataModel,n, metric, size):
+    X = data_model.df.iloc[:, :-1]
+    y = data_model.df.iloc[:, -1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=size / 100)
+    # Initialize SVM classifier
+    KNN_classifier = KNeighborsClassifier(n_neighbors=n, metric=metric)
+    # Train the classifier
+    KNN_classifier.fit(X_train, y_train)
+    # Predict on the test set
+    y_pred = KNN_classifier.predict(X_test)
+    # Calculate accuracy
+    display_evaluation_metrics(y_test, y_pred, "c")
 
 
-def display_evaluation_metrics(accuracy, precision, recall, f1, confusion_mat):
+def display_evaluation_metrics(y_test, y_pred, type):
     # Create a Tkinter window
     window = tk.Tk()
     window.geometry('600x400')  # Set window size
@@ -175,25 +105,26 @@ def display_evaluation_metrics(accuracy, precision, recall, f1, confusion_mat):
     frame = tk.Frame(window)
     frame.pack(padx=20, pady=10)
 
-    # Accuracy label
-    accuracy_label = tk.Label(frame, text="Accuracy: {:.2f}%".format(accuracy * 100))
-    accuracy_label.pack()
+    accuracy = accuracy_score(y_test, y_pred)
+    tk.Label(frame, text="Accuracy: {:.2f}%".format(accuracy * 100)).pack()
 
-    # Precision label
-    precision_label = tk.Label(frame, text="Precision: {:.2f}".format(precision))
-    precision_label.pack()
+    if type == "r":
+        precision = precision_score(y_test, y_pred)
+        tk.Label(frame, text="Precision: {:.2f}".format(precision)).pack()
 
-    # Recall label
-    recall_label = tk.Label(frame, text="Recall: {:.2f}".format(recall))
-    recall_label.pack()
+        recall = recall_score(y_test, y_pred)
+        tk.Label(frame, text="Recall: {:.2f}".format(recall)).pack()
 
-    # F1-score label
-    f1_label = tk.Label(frame, text="F1-score: {:.2f}".format(f1))
-    f1_label.pack()
+        f1 = f1_score(y_test, y_pred)
+        tk.Label(frame, text="F1-score: {:.2f}".format(f1)).pack()
 
-    # Confusion matrix label
-    confusion_label = tk.Label(frame, text="Confusion Matrix:\n{}".format(confusion_mat))
-    confusion_label.pack()
+    confusion_mat = confusion_matrix(y_test, y_pred)
+    tk.Label(frame, text="Confusion Matrix:\n{}".format(confusion_mat)).pack()
 
+    if type == "c":
+
+        clas_matric=classification_report(y_test, y_pred)
+        tk.Label(frame, text="classification report:\n{}".format(clas_matric)).pack()
     # Run the Tkinter event loop
     window.mainloop()
+
